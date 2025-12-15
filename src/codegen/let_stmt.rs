c9d848ca93897ef9a62e3f085ab754abf6f09da5
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2025-12-15 23:33:30 +0545

    fix: generate alloca load pair for let statement

--- src/codegen/let_stmt.rs
+++ src/codegen/let_stmt.rs
@@ -6,7 +6,8 @@ use melior::{
 };
 use melior::dialect::{arith, llvm};
 use melior::dialect::llvm::{AllocaOptions, LoadStoreOptions};
-use melior::ir::attribute::IntegerAttribute;
+use melior::dialect::llvm::r#type::pointer;
+use melior::ir::attribute::{IntegerAttribute, TypeAttribute};
 use crate::{
     ast::{AssignStmt, LetStmt},
     codegen::expressions::compile_expr,
@@ -23,23 +24,36 @@ pub fn compile_let<'ctx: 'parent, 'parent>(
     block: &'parent Block<'ctx>,
     stmt: &LetStmt,
 ) {
+    let i64_ty = IntegerType::new(ctx.ctx, 64).into();
 
     let op = arith::constant(
         ctx.ctx,
-        IntegerAttribute::new(IntegerType::new(ctx.ctx, 32).into(), 1).into(),
+        IntegerAttribute::new(IntegerType::new(ctx.ctx, 64).into(), 1).into(),
         Location::unknown(ctx.ctx),
     );
     let op_ref = block.append_operation(op);
 
-    let value = op_ref.result(0).expect("IDK").into();
-    
-    let op = llvm::alloca(ctx.ctx, value, IntegerType::new(ctx.ctx, 32).into(), Location::unknown(ctx.ctx), AllocaOptions::default());
+    let value:Value = op_ref.result(0).expect("IDK").into();
+
+    let expr = stmt.expr.clone();
+
+    let expr_value = compile_expr(ctx, locals, block, &expr);
+
+    let op = llvm::store(ctx.ctx, expr_value, value, Location::unknown(ctx.ctx), LoadStoreOptions::default());
+
+    let i64_ptr_ty = llvm::r#type::pointer(ctx.ctx, 64);
+
+    let op = llvm::alloca(ctx.ctx, value, i64_ptr_ty, Location::unknown(ctx.ctx), AllocaOptions::new().elem_type(Some(TypeAttribute::new(i64_ty))));
 
     let op_ref = block.append_operation(op);
 
     let val = op_ref.result(0).expect("IDK").into();
 
     locals.insert(stmt.variable.clone(), val);
+
+    let op = llvm::store(ctx.ctx, expr_value, val, Location::unknown(ctx.ctx), LoadStoreOptions::default());
+
+    block.append_operation(op);
 }
 
 /// An assign statement
@@ -58,6 +72,8 @@ pub fn compile_assign<'ctx: 'parent, 'parent>(
             let expr_value = compile_expr(ctx, locals, block, &expr);
 
             let op = llvm::store(ctx.ctx, expr_value, *val, Location::unknown(ctx.ctx), LoadStoreOptions::default());
+
+            block.append_operation(op);
 
         },
         None => {}
